78b6202a16a60e9ae8dec9882c30073f2078be83
“Michael <240designworks@gmail.com>
2025-02-18 12:41:46 -0600

    Initial Commit

---
+++ SwiftCal/SwiftCalApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftCalApp.swift
+//  SwiftCal
+//
+//  Created by Michael Martell on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCalApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
